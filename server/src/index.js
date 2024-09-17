22fa21c8b054c9a76aba5fee1c89ee15366f8c70
nik6348 <rajputnik911@gmail.com>
2024-09-17 10:32:47 +0530

    add routes and db config

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,21 @@
-console.log("Hello world!");
+import express from 'express';
+import studentRoutes from './routes/student.routes.js';
+import interviewRoutes from './routes/interview.routes.js';
+import { connectToDB } from './config/db.js';
+import cors from 'cors';
+
+//  Initialize express app
+const app = express();
+app.use(express.json()); // Parse JSON request bodies
+app.use(cors())
+
+// Connect to MongoDB
+connectToDB()
+
+// Use routes
+app.use('/api/students', studentRoutes);
+app.use('/api/interviews', interviewRoutes);
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
